f6541fe2263972d5d2032e8fe8b7c0fe8c7b0ed6
BrunoB <b.buyst@outlook.be>
2020-10-01 19:29:29 +0200

    Manipulation DOM JQuery

--- bases/js/main.js
+++ bases/js/main.js
@@ -1,21 +1,21 @@
 const myButton = document.getElementById('bouton1');
 
-if(myButton != null) { //si l'élément du dom existe (si pas il renvoie null)'
+if (myButton != null) { //si l'élément du dom existe (si pas il renvoie null)'
     let toggle = '1';
-    myButton.addEventListener('click', function(){
+    myButton.addEventListener('click', function () {
         /* alert('Message') */
-        if(toggle === '1') {
+        if (toggle === '1') {
             myButton.classList.remove('btn-success');
-            myButton.classList.add('btn-danger');
-            myButton.textContent = 'Bouton Rouge';
+            myButton.classList.add('btn-warning');
+            myButton.textContent = 'Bouton Jaune';
             toggle = '2';
-        } 
+        }
         else if (toggle === '2') {
-            myButton.classList.remove('btn-danger');
+            myButton.classList.remove('btn-warning');
             myButton.classList.add('btn-success');
             myButton.textContent = 'Bouton Vert';
             toggle = '3';
-        }else if (toggle === '3') {
+        } else if (toggle === '3') {
             myButton.classList.remove('btn-success');
             myButton.classList.add('btn-primary');
             myButton.textContent = 'Bouton Bleu';
@@ -25,8 +25,8 @@ if(myButton != null) { //si l'élément du dom existe (si pas il renvoie null)'
 }
 
 let display = 1;
-document.getElementById('bouton2').addEventListener('click', function() {
-    if(display === 1) {
+document.getElementById('bouton2').addEventListener('click', function () {
+    if (display === 1) {
         document.getElementsByTagName('table')[0].style = 'display: none;';
         display = 2;
     } else if (display === 2) {
@@ -34,13 +34,17 @@ document.getElementById('bouton2').addEventListener('click', function() {
         display = 1;
     }
 });
+
+
 /* 
-document.getElementsByClassName('btn')[0].addEventListener('click', function() { alert('getElementsByClassName'); } );
-//getElementsByClassName renvoie un tableau et on veut le 1er élément donc [0] 
+//Code en JQuery pour supprimer la dernière ligne du tableau avec le bouton 
+$('#bouton3').on('click', function () {
+    $('table').find('tr').last().remove();
+}); 
+ */
+//Code en javascript normal pour supprimer la dernière ligne du tableau avec le bouton  
+document.getElementById('bouton3').addEventListener('click', function () {
+    document.getElementsByTagName('tbody')[0].lastElementChild.remove();
+    //
+});
 
- 
-console.log('Ceci est un message de test');
-console.warn('Ceci est un message de test');
-console.error('Ceci est un message de test');  
-
-*/
